ece7742da5d37f418500151639cd8afe56f3cac6
Manuel A. Ruiz Garcia <mruiz.ve@gmail.com>
2017-07-30 20:29:03 +0200

    boh

--- scripts/pc.sh
+++ scripts/pc.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 # make package
-cd "$(dirname $0)/../build";
+cd $(dirname $0)/../build;
 make -j4
 error=$?
 cd -
 
 if [ "$error" -eq "0" ]; then
 	# launch camera and target detection in background
-	screen -dmS target "$(dirname $0)/setup-target.sh"
+	screen -dmS target $(dirname $0)/setup-target.sh
 
 	# launch dynamic reconfigure (robot master)
 	uri=($(grep "uri/robot" "$(dirname $0)/../launch/setup.launch"))
--- scripts/setup-target.sh
+++ scripts/setup-target.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # prepare ros environment
-source "$(dirname $0)/setup-environment.sh"
+source $(dirname $0)/setup-environment.sh
 
 # start camera and target detection
 roslaunch carote target.launch
