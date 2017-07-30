1727de921a9d8ab4e9f416f22f5db1c6f5d557ef
Manuel A. Ruiz Garcia <mruiz.ve@gmail.com>
2017-07-30 20:33:31 +0200

    removed controller

--- scripts/robot.sh
+++ scripts/robot.sh
@@ -2,7 +2,3 @@
 
 # launch youbot drivers in background
 screen -dmS drivers $(dirname $0)/setup-drivers.sh
-
-# launch controller in background
-screen -dmS controller $(dirname $0)/setup-controller.sh
-
