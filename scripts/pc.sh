c46e3909a2f0d24507b3659a526a28e06ebf3673
Manuel A. Ruiz Garcia <mruiz.ve@gmail.com>
2017-07-30 19:40:06 +0200

    dynamic reconfigure node (from launch to script)

--- scripts/pc.sh
+++ scripts/pc.sh
@@ -5,3 +5,7 @@ screen -dmS target $(dirname $0)/setup-target.sh
 
 # launch controller in background
 screen -dmS controller $(dirname $0)/setup-controller.sh
+
+# launch dynamic reconfigure (robot master)
+rosscooby
+rosrun rqt_reconfigure rqt_reconfigure
