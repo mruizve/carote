e02f64f978820985db5ea7a34faee14b31b4b927
Manuel A. Ruiz Garcia <mruiz.ve@gmail.com>
2017-07-30 20:17:50 +0200

    string completion fix

--- scripts/setup-environment.sh
+++ scripts/setup-environment.sh
@@ -3,10 +3,10 @@
 # prepare ros environment
 source "/opt/ros/indigo/setup.bash"
 
-if [ -f "~/catkin_ws/devel/setup.bash" ]; then
-	source "~/catkin_ws/devel/setup.bash"
+if [ -f ~/catkin_ws/devel/setup.bash ]; then
+	source ~/catkin_ws/devel/setup.bash
 fi
 
-if [ -f "~/workspace/ros/devel/setup.sh" ]; then
-	source "~/workspace/ros/devel/setup.sh"
+if [ -f ~/workspace/ros/devel/setup.sh ]; then
+	source ~/workspace/ros/devel/setup.sh
 fi
