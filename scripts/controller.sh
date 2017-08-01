629434f9788334f9ad330a094b0ff49900649fd8
Manuel A. Ruiz Garcia <mruiz.ve@gmail.com>
2017-08-01 10:11:43 +0200

    added make rule and dynamic reconfigure client

--- scripts/controller.sh
+++ scripts/controller.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
-# launch controller in background
-screen -dmS controller $(dirname $0)/setup-controller.sh
+# make package
+cd $(dirname $0)/../build;
+make -j4
+error=$?
+cd -
 
+if [ "$error" -eq "0" ]; then
+	# launch controller in background
+	screen -dmS controller $(dirname $0)/setup-controller.sh
+
+	# launch dynamic reconfigure (robot master)
+	uri=($(grep "uri/robot" "$(dirname $0)/../launch/setup.launch"))
+	uri=${uri[3]//\"}
+	uri=${uri#value\=}
+	export ROS_MASTER_URI="$uri"
+	rosrun rqt_reconfigure rqt_reconfigure
+fi
