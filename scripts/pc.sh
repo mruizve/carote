1a3d166088292f00eb47b28f5234982ebfdfcea9
Manuel A. Ruiz Garcia <mruiz.ve@gmail.com>
2017-08-01 10:11:57 +0200

    removed controller stuff

--- scripts/pc.sh
+++ scripts/pc.sh
@@ -9,11 +9,4 @@ cd -
 if [ "$error" -eq "0" ]; then
 	# launch camera and target detection in background
 	screen -dmS target $(dirname $0)/setup-target.sh
-
-	# launch dynamic reconfigure (robot master)
-	uri=($(grep "uri/robot" "$(dirname $0)/../launch/setup.launch"))
-	uri=${uri[3]//\"}
-	uri=${uri#value\=}
-	export ROS_MASTER_URI="$uri"
-	rosrun rqt_reconfigure rqt_reconfigure
 fi
