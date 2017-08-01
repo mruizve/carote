b8266eec6ee15aca4da6950126883b20c73b7698
Manuel A. Ruiz Garcia <mruiz.ve@gmail.com>
2017-08-01 12:19:19 +0200

    automatic setup of the ros master uri

--- scripts/setup-controller.sh
+++ scripts/setup-controller.sh
@@ -3,5 +3,11 @@
 # prepare ros environment
 source $(dirname $0)/setup-environment.sh
 
+# connect to the robot ros master
+uri=($(grep "uri/robot" "$(dirname $0)/../launch/setup.launch"))
+uri=${uri[3]//\"}
+uri=${uri#value\=}
+export ROS_MASTER_URI="$uri"
+
 # start camera and target detection
 roslaunch carote controller.launch
