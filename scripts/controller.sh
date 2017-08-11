3f8787722eccd193ddc5ec68157cc16e62cc1703
Manuel A. Ruiz Garcia <mruiz.ve@gmail.com>
2017-08-11 17:09:05 +0200

    removed gui node

--- scripts/controller.sh
+++ scripts/controller.sh
@@ -2,13 +2,3 @@
 
 # launch controller in background
 screen -dmS controller $(dirname $0)/setup-controller.sh
-
-# prepare ros environment
-source $(dirname $0)/setup-environment.sh
-
-# launch dynamic reconfigure (robot master)
-uri=($(roscd carote/launch; grep "uri/robot" "setup.launch"))
-uri=${uri[3]//\"}
-uri=${uri#value\=}
-export ROS_MASTER_URI="$uri"
-#rosrun rqt_reconfigure rqt_reconfigure
