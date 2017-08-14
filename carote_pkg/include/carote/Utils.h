94d0ddd350a9a5437fe6a43c1896788c14c8940a
Manuel A. Ruiz Garcia <mruiz.ve@gmail.com>
2017-08-14 12:42:49 +0200

    added the pinv (pseudo-inverse) method

--- carote_pkg/include/carote/Utils.h
+++ carote_pkg/include/carote/Utils.h
@@ -1,12 +1,9 @@
 #ifndef _CAROTE_UTILS_H_
 #define _CAROTE_UTILS_H_
 
+#include<Eigen/SVD>
+#include<Eigen/Geometry>
 #include<ros/ros.h>
-
-template<typename T> int sgn(T val)
-{
-	return (T(0)<val)-(val<T(0));
-}
 
 #define CAROTE_NODE_ABORT(stream) do \
 { \
@@ -14,5 +11,44 @@ template<typename T> int sgn(T val)
 	exit(EXIT_SUCCESS); \
 } while(0)
 
+// sign function with sgn(0)=0
+template<typename T> int sgn(T val)
+{
+	return (T(0)<val)-(val<T(0));
+}
+
+//compute the matrix pseudo inverse using the Eigen library
+namespace Eigen
+{
+	template<typename _T, int _rows, int _cols>
+	Matrix<_T,_cols,_rows> pinv(const Matrix<_T,_rows,_cols>& _m, const double _eps)
+	{
+		// compute the SVD and get the singular values
+		typename JacobiSVD< Matrix<_T,_rows,_cols> >::SingularValuesType sigma;
+		JacobiSVD< Matrix<_T,_rows,_cols> > svd(_m,ComputeFullU|ComputeFullV);
+		sigma=svd.singularValues();
+
+		//compute the inverse of the singular values avoiding kinematics singularities
+		for(unsigned int i=0;sigma.size()>i;i++)
+		{
+			if( 5.0*_eps<sigma(i) )
+			{
+				sigma(i)=1.0/sigma(i);
+			}
+			else
+			{
+				sigma(i)=1.0/(_eps*_eps+sigma(i)*sigma(i));
+			}
+		}
+
+		// generate the diagonal matrix of singular values
+		Matrix<_T,_rows,_cols> Sigma;
+		Sigma.setZero();
+		Sigma.diagonal()=sigma;
+
+		//compute the pseudo inverse psi = V * Sigma^-1 * U^T
+		return svd.matrixV()*Sigma.transpose()*svd.matrixU().transpose();
+	}
+}
 
 #endif
