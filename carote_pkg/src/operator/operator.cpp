1d542a21e443ad3af663e78a4529a60a90200543
Manuel A. Ruiz Garcia <mruiz.ve@gmail.com>
2017-08-20 11:11:43 +0200

    operator updated 1. added missing auto stop. 2. trajectory generated with constant speed (the speed should be defined as a parameter).

--- carote_pkg/src/operator/operator.cpp
+++ carote_pkg/src/operator/operator.cpp
@@ -5,8 +5,7 @@
 carote::Operator::Operator(const std::string& _name)
 :
 	node_("~"),
-	name_(_name),
-	period_(0.02)
+	name_(_name)
 {
 	std::string strpar;
 
@@ -31,11 +30,21 @@ void carote::Operator::cbAuto(const ros::TimerEvent& _event)
 {
 	static double t=0.0;
 
+	double w;
+	if( 0.0!=config_.phi )
+	{
+		w=0.04/(config_.rho*sin(config_.phi));
+	}
+	else
+	{
+		w=0.0;
+	}
+
 	// prepare operator message
 	carote_msgs::OperatorStamped msg;
 	msg.header.stamp=ros::Time::now();
-	msg.data.lambda=config_.lambda*cos(2.0*M_PI*0.020*t);
-	msg.data.phi=config_.phi*sin(4.0*M_PI*0.015*t);
+	msg.data.lambda=config_.phi*sin(w*t);
+	msg.data.phi=config_.phi*cos(w*t);
 	msg.data.rho=config_.rho;
 	msg.data.z_lower=config_.z_lower;
 	msg.data.z_upper=config_.z_upper;
@@ -52,6 +61,8 @@ void carote::Operator::cbReconfigure(carote::OperatorConfig& _config, uint32_t _
 	
 	if( !config_.automatic )
 	{
+		timer_.stop();
+		
 		// prepare operator message
 		carote_msgs::OperatorStamped msg;
 		msg.header.stamp=ros::Time::now();
@@ -67,6 +78,7 @@ void carote::Operator::cbReconfigure(carote::OperatorConfig& _config, uint32_t _
 	else
 	{
 		// start internal timer
+		period_=ros::Duration(1.0/config_.rate);
 		timer_=node_.createTimer(period_,&Operator::cbAuto,this);
 	}
 }
